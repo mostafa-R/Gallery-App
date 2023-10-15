f74056f3734b5327f0982d9ae87e1e700a4808d9
Mostafa Ramadan <123526164+mostafa-R@users.noreply.github.com>
2023-10-15 23:41:43 +0300

    Update index.js

--- backEnd/index.js
+++ backEnd/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use("*/images", express.static(__dirname + "/public/images"));
 
 
 mongoose.connect(process.env.DB_URL, {
